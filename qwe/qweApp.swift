17cbaee7cddfa7de7713552e2c41e8bf0c66baab
Emir Kandemir <emirkagankandemir@gmail.com>
2023-10-29 11:48:16 +0300

    Initial Commit

---
+++ qwe/qweApp.swift
@@ -0,0 +1,17 @@
+//
+//  qweApp.swift
+//  qwe
+//
+//  Created by Emir Kandemir on 29.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct qweApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
